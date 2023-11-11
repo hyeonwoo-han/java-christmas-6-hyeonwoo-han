e78d34ae874871d685311769ecb9271f516add4e
hyeonwoo <ziczin7176@gmail.com>
2023-11-11 14:46:26 +0900

    feat: implement Menu constants

---
+++ src/main/java/christmas/constants/Appetizer.java
@@ -0,0 +1,24 @@
+package christmas.constants;
+
+public enum Appetizer {
+    BUTTON_MUSHROOM_SOUP("양송이수프", 6000),
+    TAPAS("타파스", 5500),
+    CEASER_SALAD("시저샐러드", 8000);
+
+
+    private final String name;
+    private final int price;
+
+    Appetizer(String name, int price) {
+        this.name = name;
+        this.price = price;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+}
---
+++ src/main/java/christmas/constants/MainDish.java
@@ -0,0 +1,24 @@
+package christmas.constants;
+
+public enum MainDish {
+    T_BONE_STEAK("티본스테이크", 55000),
+    BARBEQUE_RIB("바비큐립", 54000),
+    SEAFOOD_PASTA("해산물파스타", 35000),
+    CHRISTMAS_PASTA("크리스마스파스타", 25000);
+
+    private final String name;
+    private final int price;
+
+    MainDish(String name, int price) {
+        this.name = name;
+        this.price = price;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+}
