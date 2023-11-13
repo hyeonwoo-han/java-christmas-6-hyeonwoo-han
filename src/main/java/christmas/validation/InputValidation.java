d6fa35657899bab64b26669eb6606b8ee429a003
hyeonwoo <ziczin7176@gmail.com>
2023-11-13 10:54:21 +0900

    test: add order validation test

--- src/main/java/christmas/validation/InputValidation.java
+++ src/main/java/christmas/validation/InputValidation.java
@@ -1,15 +1,45 @@
 package christmas.validation;
 
 import christmas.constants.ErrorMessage;
+import java.util.Arrays;
+import java.util.List;
 
 public class InputValidation {
 
 
-    public void validEventDate(String input) {
+    public void validateEventDate(String input) {
         int date = convertIntoNumber(input);
         if (date < 1 || date > 31) {
             throw new IllegalArgumentException(ErrorMessage.DATE_ERROR.getContent());
         }
+    }
+
+    public void validateOrder(String input) {
+        List<String> menuAndCount = Arrays.asList(input.split(","));
+    }
+
+    private void validateOrderFormat() {
+
+    }
+
+    private void checkMenu() {
+
+    }
+
+    private void isMenuMoreThanOne() {
+
+    }
+
+    private void hasDuplicatedMenu() {
+
+    }
+
+    private void isNotOnlyDrink() {
+
+    }
+
+    private void isMenuLessThanTwentyOne() {
+
     }
 
     private int convertIntoNumber(String input) {
--- src/test/java/christmas/validationTest/InputValidationTest.java
+++ src/test/java/christmas/validationTest/InputValidationTest.java
@@ -15,8 +15,18 @@ public class InputValidationTest {
     @ValueSource(strings = {"삼십일", "32", " ", "2023.12.12", "2023-12-12"})
     @DisplayName("날짜 검증 테스트")
     public void validDateTest(String input) {
-        assertThatThrownBy(() -> inputValidation.validEventDate(input))
+        assertThatThrownBy(() -> inputValidation.validateEventDate(input))
                 .isExactlyInstanceOf(IllegalArgumentException.class)
                 .hasMessage("[ERROR] 유효하지 않은 날짜입니다. 다시 입력해 주세요.");
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {"해산물파스타:2,레드와인:2", "알리오올리오:1,타파스:2", "해산물파스타:0,레드와인:2", "해산물파스타:2,해산물파스타:1,레드와인1", "레드와인:1",
+            "해산물파스타:15,레드와인:6"})
+    @DisplayName("주문 메뉴 검증 테스트")
+    public void validateOrderTest(String input) {
+        assertThatThrownBy(() -> inputValidation.validateOrder(input))
+                .isExactlyInstanceOf(IllegalArgumentException.class)
+                .hasMessage("[ERROR] 유효하지 않은 주문입니다. 다시 입력해 주세요.");
     }
 }
