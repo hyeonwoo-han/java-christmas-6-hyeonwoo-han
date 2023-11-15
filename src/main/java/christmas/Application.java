07bfa2febdab42f508a4fb79c7ef087e4db39f2e
hyeonwoo <ziczin7176@gmail.com>
2023-11-15 23:05:38 +0900

    feat: Implement event planner runner

--- src/main/java/christmas/Application.java
+++ src/main/java/christmas/Application.java
@@ -1,7 +1,16 @@
 package christmas;
 
+import christmas.controller.ChristmasController;
+import christmas.validation.InputValidation;
+import christmas.view.InputView;
+import christmas.view.OutputView;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        InputView inputView = new InputView(new InputValidation());
+        OutputView outputView = new OutputView();
+        ChristmasController christmasController = new ChristmasController(inputView, outputView);
+        christmasController.runEventPlanner();
     }
 }
