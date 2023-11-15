ada5fc3beb76be0574360f41c0ff188136d9bac4
hyeonwoo <ziczin7176@gmail.com>
2023-11-15 20:33:46 +0900

    feat: Implement print order

--- src/main/java/christmas/view/OutputView.java
+++ src/main/java/christmas/view/OutputView.java
@@ -1,6 +1,7 @@
 package christmas.view;
 
 import christmas.constants.Message;
+import java.util.Map;
 
 public class OutputView {
     public void sayGreeting() {
@@ -17,5 +18,13 @@ public class OutputView {
 
     public void previewEventBenefit(int visitDate) {
         System.out.println(Message.PREVIEW_EVENT_BENEFIT.getEventPreviewMsg(visitDate));
+    }
+
+    public void confirmOrder(Map<String, Integer> menusAndCounts) {
+        System.out.println(Message.ORDERED_MENU_INFORM);
+        for (String menu : menusAndCounts.keySet()) {
+            int count = menusAndCounts.get(menu);
+            System.out.println(menu + " " + count + "개");
+        }
     }
 }
